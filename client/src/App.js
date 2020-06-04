416b47d507490b7766695813582c15f4226aed4d
pat-white-code <pat.k.white@gmail.com>
2020-06-04 16:00:38 -0500

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios'
+
+function App() { 
+  const handleTest = () => {
+    axios.get('/test').then(res => console.log(res))
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={handleTest}>
+          test server
+        </button>
+        <h2>Updated!</h2>
+        <h3>More updates!</h3>
+        <a>
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
